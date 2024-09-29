f9ca817217327e548bcc8e8420e97dde47a465a9
Shanshan Fu <jesse.fu21@gmail.com>
2024-09-29 22:10:00 +0100

    watch list 1

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :lists, except: [ :edit, :update ] do
+    resources :bookmarks, only: [ :new, :create, :destroy ]
+  end
 end
